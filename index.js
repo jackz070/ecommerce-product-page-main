0ae7fb315977c36c4059f3a5e95bd60cb811477a
jackz070 <95173354+jackz070@users.noreply.github.com>
2022-09-14 16:06:38 +0200

    JS fixes, comments
    - Wrote cart popup functionality from scratch to fix bugs; simpler approach is much better; praise event.stopPropagation()!
    - Added comments to index.js
    - Added comments to CSS

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Selectors would me much clearer if stated in relevant parts of the code, where they are needed, not on top
+
 const thumbnails = document.querySelectorAll(".main__gallery-thumbnails img");
 const largeImage = document.querySelector(".main__gallery-large img");
 const addToCartButton = document.querySelector(".addToCartButton");
@@ -19,9 +21,11 @@ const inventory = {
   },
 };
 
+// Declaring cart as an empty array
+
 const cart = [];
 
-// Gallery element
+// Gallery thumbnails functionality
 
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener("click", () => {
@@ -31,7 +35,7 @@ thumbnails.forEach((thumbnail) => {
   });
 });
 
-// Counter
+// Cart button counter bubble
 
 const itemCountMinus = () => {
   if (itemCount.innerText > 0) itemCount.innerText--;
@@ -41,7 +45,7 @@ const itemCountPlus = () => {
   itemCount.innerText++;
 };
 
-// Cart Functionality
+// Cart Functionality for display purpose, assuming that we have one item only.
 
 const handleAddToCart = () => {
   let toAdd = { ...inventory[1], count: parseInt(itemCount.innerText) };
@@ -55,9 +59,11 @@ const handleAddToCart = () => {
     navbarCartBubble.classList.remove("hidden");
     itemCount.innerText = 0;
   }
+  //In case it was possible to add item to cart the popup is visible. Atm the popup closes whenever anything out of it is closed
   populateCartPopup();
 };
 
+//cart item delete function remove the only item we have AND hides the counter bubble
 const cartItemDelete = () => {
   cart.splice(0, 1);
   populateCartPopup();
@@ -66,25 +72,33 @@ const cartItemDelete = () => {
 
 //Cart display
 
-const toggleShowCart = () => {
+// Step 1. function to show the cart popup: event.stopPropagation() stops the listener from triggering on other elements that are higher in structure and allows me not to close the popup immediately by triggering the hide cart listener on window; remove class "hidden" and populate the popup; remove event listener from the button so that it can be used to close the popup
+
+const showCart = (e) => {
+  e.stopPropagation();
   navbarCartPopup.classList.remove("hidden");
   populateCartPopup();
-
-  setTimeout(() => {
-    window.addEventListener("click", function handleShow(e) {
-      let deleteButton = document.querySelector(".delete");
-      if (
-        e.target !== navbarCartPopup &&
-        !navbarCartPopup.contains(e.target) &&
-        e.target !== deleteButton
-      ) {
-        console.log(e);
-        navbarCartPopup.classList.add("hidden");
-        window.removeEventListener("click", handleShow);
-      }
-    });
-  }, 100);
+  navbarCartButton.removeEventListener("click", showCart);
 };
+
+navbarCartButton.addEventListener("click", showCart);
+
+//Step 2. adding stopPropagation() to the popup itself stops it from triggering the listener that closes the popup (on the window, never to be reached)
+
+navbarCartPopup.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+//Step 3. Add the hide cart event listener to window, which triggers a function that adds "hidden" class to popup and re-adds the show cart event listener to the cart button
+
+const hideCart = (e) => {
+  navbarCartPopup.classList.add("hidden");
+  navbarCartButton.addEventListener("click", showCart);
+};
+
+window.addEventListener("click", hideCart);
+
+// Populate cart popup functions is triggered every time the cart popup is displayed to check the contents of cart: if it contains anything, it will get displayed, if there is nothing a message stating this appears
 
 const populateCartPopup = () => {
   if (cart.length === 0) {
@@ -103,39 +117,7 @@ const populateCartPopup = () => {
   }
 };
 
-// Gallery modal
-
-// const modalClose = () => {
-//   galleryModal.classList.add("hidden");
-// };
-// const modalShow = () => {
-//   galleryModal.classList.remove("hidden");
-//   activeModalImage = modalThumbnails[0];
-// };
-
-// modalThumbnails.forEach((thumbnail) => {
-//   thumbnail.addEventListener("click", () => {
-//     modalThumbnails.forEach((thumbnail) =>
-//       thumbnail.classList.remove("active")
-//     );
-//     thumbnail.classList.toggle("active");
-//     setActiveModalImage();
-//     galleryModalLarge.src = thumbnail.src.replace("-thumbnail", "");
-//   });
-// });
-
-// setActiveModalImage = () => {
-//   let activeModalImage = Array.prototype.find.call(modalThumbnails, (item) =>
-//     item.classList.contains("active")
-//   );
-//   galleryModalLarge.src = activeModalImage.src.replace("-thumbnail", "");
-// };
-
-// const prevImage = () => {};
-
-// const nextImage = () => {};
-
-// Gallery modal approach 2
+// Gallery modal (seconds approach, first one was much more convoluted, longer and buggy)
 
 const galleryModal = document.querySelector(".modal__overlay");
 const galleryModalLarge = document.querySelector(".modal__gallery-large img");
@@ -204,7 +186,7 @@ let nextImage = () => {
   }
 };
 
-// Mobile gallery controls
+// Mobile-view large gallery controls
 
 const images = [
   "image-product-1.jpg",
